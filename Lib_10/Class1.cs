b86fbb4c1eae8ca1f9e2aa6f8ad8c97ffcd8bd1e
ИСП-31 <-31@LAB17-10>
2021-10-05 13:33:13 +0300

    Добавьте файлы проекта.

---
+++ Lib_10/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_10
+{
+    public class Class1
+         
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="k"></param>
+        /// <param name="kol"></param>
+        /// <param name="s2"></param>
+        /// <param name="s1"></param>
+
+        public static void Sum(int k,out int kol,out string s2, out string s1)
+        {
+            int x;// сгенерированное число
+            int sum = 0;// сумма
+            kol = 0;// количество сгенерированных чисел
+            Random rnd = new Random();
+            s1 = " ";// строка для вывода суммы
+            s2 = "";// строка для вывода сгенерированных чисел
+           
+                while(sum<k)
+                {
+                    x = rnd.Next(5, 10);// генерирование чисел, распределенных в диапазоне от 5 до 10
+                s2 = x.ToString() + ";" + s2 ;//заполение строки числами
+                    kol++;// счетчик количества чисел
+                    sum = sum + x;// вычисление суммы
+                    s1 = sum.ToString();// заполнение строки суммой
+
+                }
+          
+        }
+    }
+}
---
+++ Практическая работа 1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib_10;
+
+namespace Практическая_работа_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Сброс_Click(object sender, RoutedEventArgs e)
+        {
+            Chisla.Text = null;
+            Sum.Text = null;
+            Kol.Text = null;
+            K.Text = null;
+        }
+
+        private void Опрограмме_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Мельцаева Дарья\n"+"Практическая работа №1\n" + "Вычислить сумму целых случайных чисел, распределенных в диапазоне от 5 до 10, пока эта сумма не превышает некоторого числа K. Вывести на экран сгенерированные числа, значение суммы, и количество сгенерированных чисел.");
+        }
+
+        private void Выход_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Raschet_Click(object sender, RoutedEventArgs e)
+        {
+            int k,kol;
+            k = Convert.ToInt32(K.Text);// ввод K
+            Class1.Sum(k, out kol,out string s2, out string s1);// применение функции
+            Chisla.Text = s2;// вывод сгенерированных чисел
+            Sum.Text = s1;// вывод суммы
+            Kol.Text = Convert.ToString(kol);// вывод количесва чисел
+        }
+    }
+}
